54dfbb5d6904d38c1bef8acdf8b752179d4e42f9
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-10-15 01:18:32 -0200

    Fixed BookShelf test describe

--- src/components/BookShelf.test.js
+++ src/components/BookShelf.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import BookShelf from './BookShelf';
 
-describe('Shelves Container', () => {
+describe('BookShelf Container', () => {
   it('renders correctly', () => {
     const BookShelfTree = renderer.create(<BookShelf />).toJSON();
     expect(BookShelfTree).toMatchSnapshot();
